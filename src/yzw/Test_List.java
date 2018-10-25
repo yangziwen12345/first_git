c9398c18ca403cf70d15a5d085fd2e5efcedb6a1
yangziwen12345 <360429yang1994>
2018-10-25 23:49:41 +0800

    bb

--- src/yzw/Test_List.java
+++ src/yzw/Test_List.java
@@ -1,10 +1,19 @@
 package yzw;
 
+import java.util.*;
+
 /**
  * Created by Administrator on 2018/10/25.
  */
-public class Test_List
+public class Test_List {
+    List<String> list = new ArrayList<>();
 
+    List<String> list2 = new LinkedList<>();
 
-{
+    List<String> list3 = new Vector<>();
+
+    Set<String> set = new HashSet<>();
+
+    Set<String> set2 = new TreeSet<>();
+
 }
---
+++ src/yzw/Test_Map.java
@@ -0,0 +1,19 @@
+package yzw;
+
+import java.util.*;
+
+/**
+ * Created by Administrator on 2018/10/25.
+ */
+public class Test_Map {
+
+    Map<String, String> map = new HashMap<>();
+
+    Map<String, String> map2 = new TreeMap<>();
+
+    Map<String, String> map3 = new LinkedHashMap<>();
+
+    Map<String, String> map4 = new Hashtable<>();
+
+
+}
